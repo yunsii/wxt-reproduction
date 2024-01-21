9e03b8dc0e888ae91413ecc125b431d673ac7993
Yuns <yuns.xie@qq.com>
2024-01-21 14:48:24 +0800

    chore: i18n test

--- src/entrypoints/popup/App.tsx
+++ src/entrypoints/popup/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "@/assets/react.svg";
 import wxtLogo from "@/public/wxt.svg";
 import "./App.css";
+import { Trans, t } from "@lingui/macro";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,20 +11,28 @@ function App() {
     <>
       <div>
         <a href="https://wxt.dev" target="_blank">
-          <img src={wxtLogo} className="logo" alt="WXT logo" />
+          <img src={wxtLogo} className="logo" alt={t`WXT logo`} />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} className="logo react" alt={t`React logo`} />
         </a>
       </div>
-      <h1>WXT + React</h1>
+      <h1>
+        <Trans>WXT + React</Trans>
+      </h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+        <button onClick={() => setCount((count) => count + 1)}>
+          <Trans>count is {count}</Trans>
+        </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          <Trans>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </Trans>
         </p>
       </div>
-      <p className="read-the-docs">Click on the WXT and React logos to learn more</p>
+      <p className="read-the-docs">
+        <Trans>Click on the WXT and React logos to learn more</Trans>
+      </p>
     </>
   );
 }
